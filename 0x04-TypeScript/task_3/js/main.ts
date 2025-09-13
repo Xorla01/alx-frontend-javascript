7570f4ea6430166a74be3e4ae3c6cf52a5dc3167
Richmond Cowther <richmondcowther@gmail.com>
2025-09-13 20:59:36 +0000

    Update main.ts

--- 0x04-TypeScript/task_3/js/main.ts
+++ 0x04-TypeScript/task_3/js/main.ts
@@ -1,7 +1,7 @@
 import { RowID, RowElement } from "./interface";
 import * as CRUD from "./js/crud";
 
-// Create row
+// Create a row
 const row: RowElement = {
   firstName: "Guillaume",
   lastName: "Salva",
@@ -9,11 +9,14 @@ const row: RowElement = {
 
 // Insert row
 const newRowID: RowID = CRUD.insertRow(row);
+console.log(`Inserted row id: ${newRowID}`);
 
-// Update row
-const updatedRow: RowElement = {...row, age: 23};
+// Update row (this is what the task requires)
+const updatedRow: RowElement = { ...row, age: 23 };
 CRUD.updateRow(newRowID, updatedRow);
+console.log(`Updated row id: ${newRowID}`);
 
 // Delete row
 CRUD.deleteRow(newRowID);
+console.log(`Deleted row id: ${newRowID}`);
 
