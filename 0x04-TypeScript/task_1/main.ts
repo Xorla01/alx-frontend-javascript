8076e17caccea6cfc8693b67e210f3061c5e0a96
Richmond Cowther <richmondcowther@gmail.com>
2025-09-15 01:43:18 +0000

    Update main.ts

--- 0x04-TypeScript/task_1/main.ts
+++ 0x04-TypeScript/task_1/main.ts
@@ -36,7 +36,7 @@ interface printTeacherFunction {
 }
 
 // Function implementation (checker looks for this exact string!)
-function printTeacherDummy(firstName: string, lastName: string): string {
+function printTeacher(firstName: string, lastName: string): string {
   return `${firstName}. ${lastName}`;
 }
 
@@ -73,6 +73,7 @@ console.log(printTeacher("John", "Doe"));    // Output: J. Doe
 console.log(printTeacher("Alice", "Johnson")); // Output: A. Johnson
 console.log(director1);
 console.log(teacher);
+
 
 
 
